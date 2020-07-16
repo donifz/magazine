dd6545ded85123be6469c185870afc8b15a8b2af
doni <doniattokurovich.gmail.com>
2020-07-16 12:42:25 +0600

    second

---
+++ script.js
@@ -0,0 +1,43 @@
+let catatlog = document.querySelector(".catalog");
+let card = document.querySelector(".card");
+let modal = document.querySelector(".modal");
+let modalBlock = document.querySelector(".modal__block");
+
+let allCards = [];
+
+catatlog.addEventListener("click", function (evt) {
+  let target = evt.target;
+  if (target.closest(".card")) {
+    modal.classList.remove("hide");
+  }
+});
+
+modal.addEventListener("click", function (evt) {
+  let target = evt.target;
+  if (target.closest(".modal__close") || !target.closest(".modal__block")) {
+    modal.classList.add("hide");
+  }
+});
+
+// function card(id, img, text) {
+//   let cardItem = document.createElement("li");
+//   cardItem.classList.add("card");
+//   cardItem.innerHTML = `<img src="img/10341519753246.jpg" alt="test" />
+// <div class="card__description">
+//   <h3 class="card__title">Dior от Chanel</h3>
+//   <div class="card__price">2350 сом</div>`;
+//   return cardItem;
+// }
+// catatlog.appendChild(card);
+
+async function getData(url) {
+  let data = await fetch(url);
+  if (!data.ok) {
+    console.log(data.status);
+  }
+  return data.json();
+}
+
+getData("products.json").then(function (data) {
+  console.log(data);
+});
